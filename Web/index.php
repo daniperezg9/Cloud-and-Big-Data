70332fcfe5e45c02d4b9062322a0344f74396bd5
Daniel Pérez García <daniperezg02@gmail.com>
2024-12-14 00:10:40 +0100

    CSS y más

--- Web/index.php
+++ Web/index.php
@@ -2,6 +2,10 @@
 
 $tituloPagina = 'Clash Royale';
 
-$contenidoPrincipal ="";
+$contenidoPrincipal =" <h1>Clash Royale</h1>
+<p>Clash Royale es uno de los juegos más populares de plataforma móvil.
+Se trata de un juego de estrategia en tiempo real en el que los jugadores se enfrentan con el objetivo de destruir las torres del enemigo usando cartas con diferentes características.
+
+</p>";
 
 require './includes/vistas/plantillas/plantilla.php';
